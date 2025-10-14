4681771af366bc7fe57657f18d48e135baf635a9
stackgen-automation <automation@stackgen.com>
2025-10-14 15:53:21 +0000

    Commit by Dhairya Anilbhai Dudhatra

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+  acl    = var.acl
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "acl" {
+  description = "The ACL to apply to the bucket"
+  type        = string
+  default     = "private"
+}
+
+variable "environment" {
+  description = "The environment for the bucket"
+  type        = string
+  default     = "dev"
+}
