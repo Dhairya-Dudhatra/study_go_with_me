9b5269b87e6547ebf73c3e5021244818e8037ece
stackgen-automation <automation@stackgen.com>
2025-10-24 19:00:05 +0000

    Commit by Dhairya Anilbhai Dudhatra

---
+++ tf/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with StackGen.
+You can modify your appStack -> [here](http://appcd.local/appstacks/c4d813b8-5dc7-4b73-aa9b-445a41c31a4a)
---
+++ tf/outputs.tf
@@ -0,0 +1,20 @@
+output "aws_s3_stackgen_6e777fca-4788-4cac-ad6f-293ff76e3831_arn" {
+  value     = module.stackgen_6e777fca-4788-4cac-ad6f-293ff76e3831.arn
+  sensitive = false
+}
+
+output "aws_s3_stackgen_6e777fca-4788-4cac-ad6f-293ff76e3831_bucket_name" {
+  value     = module.stackgen_6e777fca-4788-4cac-ad6f-293ff76e3831.bucket_name
+  sensitive = false
+}
+
+output "aws_s3_stackgen_6e777fca-4788-4cac-ad6f-293ff76e3831_bucket_website_endpoint" {
+  value     = module.stackgen_6e777fca-4788-4cac-ad6f-293ff76e3831.bucket_website_endpoint
+  sensitive = false
+}
+
+output "aws_s3_stackgen_6e777fca-4788-4cac-ad6f-293ff76e3831_kms_arn" {
+  value     = module.stackgen_6e777fca-4788-4cac-ad6f-293ff76e3831.kms_arn
+  sensitive = false
+}
+
