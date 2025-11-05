b97aafb2919db7704c708e6a9e291ef56d63bf3d
stackgen-automation <automation@stackgen.com>
2025-11-05 02:49:57 +0000

    Commit by Dhairya Anilbhai Dudhatra

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,18 +1,3 @@
-output "aws_athena_stackgen_2e035ccf-df65-456c-ab41-8773c675d656_arn" {
-  value     = module.stackgen_2e035ccf-df65-456c-ab41-8773c675d656.arn
-  sensitive = false
-}
-
-output "aws_athena_stackgen_2e035ccf-df65-456c-ab41-8773c675d656_database_id" {
-  value     = module.stackgen_2e035ccf-df65-456c-ab41-8773c675d656.database_id
-  sensitive = false
-}
-
-output "aws_athena_stackgen_2e035ccf-df65-456c-ab41-8773c675d656_query_id" {
-  value     = module.stackgen_2e035ccf-df65-456c-ab41-8773c675d656.query_id
-  sensitive = false
-}
-
 output "aws_s3_stackgen_bd6bdcfe-4fb9-4475-87a3-53a303d10979_arn" {
   value     = module.stackgen_bd6bdcfe-4fb9-4475-87a3-53a303d10979.arn
   sensitive = false
